fb54552522dc6652dd92ab65c41d841aca046c54
imanol99 <iayude99@gmail.com>
2022-09-07 10:34:47 +0200

    07/09 - I

--- Programas/3.- Multiproceso/4.- TresPeriodicas_CSV/Apagar_MAX30102.py
+++ Programas/3.- Multiproceso/4.- TresPeriodicas_CSV/Apagar_MAX30102.py
@@ -5,9 +5,18 @@ el sensor se queda encendido
 '''
 
 import max30102
-
+import smbus
 if __name__ == "__main__":
-    
-    sensor = max30102.MAX30102
-    sensor.shutdown()
+    Bus = smbus.S(1)  # Se selecciona el canal 1 ya que la raspi usa este 
+                      # canal para la comunicación I2C - NO USA EL 0  
+
+    Bus.write_i2c_block_data(0x57, 0x09, [0x80])
+    '''
+    El primero de los argumentos es la dirección del dispositivo.
+    El segundo es la dirección del registro en el que se quiere escribir
+    Y el tercerp, son los valores que se quieren introducir
+
+    Estos dos últimos datos se obtienen de la biblioteca del sensor 
+    MAX 30102
+    '''
     
