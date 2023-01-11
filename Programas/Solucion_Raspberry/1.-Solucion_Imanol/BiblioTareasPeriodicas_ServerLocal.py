7f8306e830c98dc0a70551f22fd41ecbd34ebd8d
imanol99 <iayude99@gmail.com>
2023-01-11 10:58:37 +0100

    11/01/2023

--- Programas/Solucion_Raspberry/1.-Solucion_Imanol/BiblioTareasPeriodicas_ServerLocal.py
+++ Programas/Solucion_Raspberry/1.-Solucion_Imanol/BiblioTareasPeriodicas_ServerLocal.py
@@ -5,11 +5,8 @@
 # otras dos tareas, y después la parte de creación del CSV, 
 # hacerla con varaibles internas
 
-from queue import Queue
-from time import time, sleep
+import time
 from multiprocessing import Event
-
-    
 
 def Tarea_Periodica_Sensores_3Arr (tiempo_inicio, Identificador, Periodo, Plazo, Funcion, array1, array2, arrayT):
     
@@ -28,10 +25,10 @@ def Tarea_Periodica_Sensores_3Arr (tiempo_inicio, Identificador, Periodo, Plazo,
             next_time = next_time + Periodo
             time.sleep(next_time - time.time())
             
-            if len(array1) == 3000:
-                print ("---------------- Tarea POX  -------------------")
-                print("Tarea ", Identificador, ": ", len(array1))
-        print("Tiempo Tardado en la Tarea ", Identificador," = ", time.time()-tiempo_inicio-0.1)
+            #if len(array1) == 3000:
+            #    print ("---------------- Tarea POX  -------------------")
+            #    print("Tarea ", Identificador, ": ", len(array1))
+        print(Identificador," = ", time.time()-tiempo_inicio-0.1) #"Tiempo Tardado en la Tarea ", 
         
     
 def Tarea_Periodica_Sensores_2Arr (tiempo_inicio, Identificador, Periodo, Plazo, Funcion, array1, arrayT):
@@ -43,72 +40,37 @@ def Tarea_Periodica_Sensores_2Arr (tiempo_inicio, Identificador, Periodo, Plazo,
         for i in range(0, 1200): #################
             Funcion(array1, arrayT)
             t_prueba=time.time()
-            if (time.time()-next_time) > Plazo:
+            if (time.time()-next_time) >= Plazo:
                 
                 error = error + 1
                 print(" Abort:T", Identificador)
                 if error == 5:
                     break
-            
             next_time = next_time + Periodo
             time.sleep(next_time - time.time())
             
-            if len(array1) == 1200:
-                print ("---------------- Tarea GSR  -------------------")
-                print("Tarea ", Identificador, ": ", len(array1))
-        print("Tiempo Tardado en la Tarea ", Identificador," = ", time.time()-tiempo_inicio-0.1)
-
-def Tarea_Periodica_Sensores_1Arr (tiempo_inicio, Identificador, Periodo, Plazo, Funcion, Queue):
-    
-    sleep(0.1 - (time() - tiempo_inicio))
-    next_time = time()
-    error = 0
-    i = 0
-    while 1:
-            i = i + 1
-            Funcion(Queue)
-            # if (time()-next_time) > Plazo:
-            #     print("Fuera de plazo real")
-            #     break
-
-            # if i == 100:
-            #     print ("---------------- Tarea POX  -------------------")
-            #     print("Tiempo Tardado en la Tarea ", Identificador," = ", time()-tiempo_inicio-0.1)
-            #     i = 0
-            next_time = next_time + Periodo
-            try:
-                sleep(next_time - time())  
-            except(Exception):
-                print("Fuera de plazo falso --> Tarea ", Identificador)
-                break
-            # if len(Queue) == 50:
-            #     print ("---------------- Tarea GSR  -------------------")
-            #     print("Tarea ", Identificador, ": ", len(Queue))
-
-
-
-
-
+            #if len(array1) == 1200:
+             #   print ("---------------- Tarea GSR  -------------------")
+             #   print( Identificador, ": ", len(array1))
+        print(Identificador," = ", time.time()-tiempo_inicio-0.1) #"Tiempo Tardado en la Tarea ", 
 
 def Tarea_Periodica_Sensores_5Arr_Duerme_Ini (tiempo_inicio, Identificador, Periodo, Plazo, Funcion, array1, array2, arrayT, array3, array2T):
     
-    time.sleep(0.1 - (time.time() - tiempo_inicio) + Periodo)
+    time.sleep(0.15 - (time.time() - tiempo_inicio) + Periodo)
     next_time = time.time()
     error = 0
     while 1:
             t_prueba=time.time()
             ar1 = array1[:]
-            ar2 = array2[:]
-            arT1 = arrayT[:]
-            ar3 = array3[:]
-            arT2 = array2T[:]
             array1[:] = []
+            ar2 = array2[:]
             array2[:] = []
+            arT1 = arrayT[:]
             arrayT[:] = []
+            ar3 = array3[:]
             array3[:] = []
+            arT2 = array2T[:]
             array2T[:] = []
-
-            print ("---------------- Tarea Coms -------------------")
             Funcion(ar1, ar2, arT1, ar3, arT2)
             if (time.time()-next_time) > Plazo:
             
@@ -119,4 +81,26 @@ def Tarea_Periodica_Sensores_5Arr_Duerme_Ini (tiempo_inicio, Identificador, Peri
                     break
             next_time = next_time + Periodo
             time.sleep(next_time - time.time())
-            print("Tiempo Tardado en la Tarea ", Identificador," = ", time.time()-t_prueba)
+            print(Identificador," = ", time.time()-tiempo_inicio-0.1) #"Tiempo Tardado en la Tarea ", 
+
+def Tarea_Periodica_SubidaDatos(tiempo_inicio, Identificador, Periodo, Plazo, Funcion):
+    time.sleep(0.1 - (time.time() - tiempo_inicio))
+    next_time = time.time()
+    error = 0
+    while 1:
+        for i in range(0, 1200): #################
+            Funcion()
+            t_prueba=time.time()
+            if (time.time()-next_time) >= Plazo:
+                
+                error = error + 1
+                print(" Abort:T", Identificador)
+                if error == 5:
+                    break
+            next_time = next_time + Periodo
+            time.sleep(next_time - time.time())
+            
+            #if len(array1) == 1200:
+             #   print ("---------------- Tarea GSR  -------------------")
+             #   print( Identificador, ": ", len(array1))
+        print(Identificador," = ", time.time()-tiempo_inicio-0.1) #"Tiempo Tardado en la Tarea ", 
