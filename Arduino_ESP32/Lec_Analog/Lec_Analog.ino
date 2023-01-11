e8c45297295199b06f52eb3e61c5a27fc3731418
Imanol99 <100857536+Imanol99@users.noreply.github.com>
2023-01-11 12:36:59 +0100

    11/01/2023 - Arduino

--- Arduino_ESP32/Lec_Analog/Lec_Analog.ino
+++ Arduino_ESP32/Lec_Analog/Lec_Analog.ino
@@ -10,9 +10,9 @@ void setup()
 void loop() 
 {
   int vLeido = analogRead(36);
-  float valorReal = vLeido * (3.3/4096);
+  float valorReal = vLeido * (3.3/4096)/39.3142;
 
-  float vResis = (5.0/valorReal+1)*4700*2;
+  float vResis = (3.3/valorReal+1)*4700*2;
 
   float vSiem = (1/vResis)*1000000;
   Serial.println(vSiem);
